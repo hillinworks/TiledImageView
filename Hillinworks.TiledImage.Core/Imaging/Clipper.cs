7e679ebd713f89eda4ffe22f0214446a37b051aa
hillin <hillinsilence@gmail.com>
2018-09-26 20:43:46 +0800

    add Clipper

---
+++ Hillinworks.TiledImage.Core/Imaging/Clipper.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Hillinworks.TiledImage.Utilities;
+
+namespace Hillinworks.TiledImage.Imaging
+{
+    public static class Clipper
+    {
+        private static unsafe void ClearBackBuffer(this WriteableBitmap bitmap)
+        {
+            var length = bitmap.PixelHeight * bitmap.BackBufferStride;
+            var pBuffer = (byte*)bitmap.BackBuffer;
+            for (var i = 0; i < length; ++i, ++pBuffer)
+            {
+                *pBuffer = 0xff;
+            }
+        }
+
+        public static async Task<BitmapSource> Clip(this TiledImageSource imageSource, Int32Rect bounds, int layer, int lodLevel)
+        {
+            var dimensions = imageSource.Dimensions.AtLODLevel(lodLevel);
+
+            var tileWidth = dimensions.TileWidth;
+            var tileHeight = dimensions.TileHeight;
+            var tileIndexLeft = bounds.X / tileWidth;
+            var tileIndexTop = bounds.Y / tileHeight;
+            var tileIndexRight = (bounds.X + bounds.Width - 1) / tileWidth;
+            var tileIndexBottom = (bounds.Y + bounds.Height - 1) / tileHeight;
+
+            var image = new WriteableBitmap(bounds.Width, bounds.Height, 96, 96, PixelFormats.Bgr24, null);
+            image.ClearBackBuffer();
+
+            for (var row = tileIndexTop; row <= tileIndexBottom; ++row)
+            {
+                var tileTop = row * tileHeight;
+                var tileBottom = (row + 1) * tileHeight;
+
+                var copyMetricsY = ImageCopyMetrics.Calculate(tileTop, tileBottom, bounds.Y, bounds.Height);
+
+                for (var column = tileIndexLeft; column <= tileIndexRight; ++column)
+                {
+                    var tileLeft = column * tileWidth;
+                    var tileRight = (column + 1) * tileWidth;
+
+                    var index = new TileIndex.Full(column, row, layer, lodLevel);
+                    var tileImage = await imageSource.LoadTileAsync(index);
+
+                    var copyMetricsX = ImageCopyMetrics.Calculate(tileLeft, tileRight, bounds.X, bounds.Height);
+
+                    var sourceRect = new Int32Rect(
+                        copyMetricsX.Source,
+                        copyMetricsY.Source,
+                        copyMetricsX.Size,
+                        copyMetricsY.Size);
+
+                    var destinationRect = new Int32Rect(
+                        copyMetricsX.Destination,
+                        copyMetricsY.Destination,
+                        copyMetricsX.Size,
+                        copyMetricsY.Size);
+
+                    tileImage.CopyPixels(
+                        sourceRect,
+                        image.BackBuffer + destinationRect.Y * image.BackBufferStride + destinationRect.X * 3,
+                        image.PixelHeight * image.BackBufferStride,
+                        image.BackBufferStride);
+                }
+            }
+
+            image.Freeze();
+            return image;
+        }
+    }
+}
---
+++ Hillinworks.TiledImage.Core/Utilities/ImageCopyMetrics.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+
+namespace Hillinworks.TiledImage.Utilities
+{
+    internal struct ImageCopyMetrics
+    {
+        private ImageCopyMetrics(int source, int destination, int size)
+        {
+            this.Source = source;
+            this.Destination = destination;
+            this.Size = size;
+        }
+
+        public int Source { get; }
+        public int Destination { get; }
+        public int Size { get; }
+
+        public static ImageCopyMetrics Calculate(
+            int sourceLowerBound,
+            int sourceUpperBound,
+            int destinationPosition,
+            int destinationSize)
+        {
+            int source, destination, size;
+
+            if (destinationPosition < sourceLowerBound)
+            {
+                source = 0;
+                destination = sourceLowerBound - destinationPosition;
+                size = destinationSize - destination;
+            }
+            else if (destinationPosition + destinationSize > sourceUpperBound)
+            {
+                size = sourceUpperBound - destinationPosition;
+                destination = 0;
+                source = destinationPosition - sourceLowerBound;
+            }
+            else
+            {
+                size = destinationSize;
+                destination = 0;
+                source = destinationPosition - sourceLowerBound;
+            }
+
+            var sourceSize = sourceUpperBound - sourceLowerBound;
+
+            if (size > sourceSize)
+            {
+                size = sourceSize;
+            }
+
+            Debug.Assert(size >= 0);
+
+            return new ImageCopyMetrics(source, destination, size);
+        }
+    }
+}
